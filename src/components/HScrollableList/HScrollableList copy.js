44e4bf716e8dd21f9dc51fecbe6573aba8d88deb
Jesus Gutierrez <jgutierrez9000@gmail.com>
2025-08-19 23:00:01 -0500

    1.6.1 Fixed Portfolio missing link.

--- src/components/HScrollableList/HScrollableList copy.js
+++ src/components/HScrollableList/HScrollableList copy.js
@@ -10,7 +10,7 @@ const projects = [
     { name: "Encrypted Drone Protocol", description: "St. Mary's University Drone Lab requested and receieved a fully custom encrypted UAV communications protocol.", photo:"/ProjectImages/radio.png", codeLink: "https://github.com/jesus900000000/UAVPROJECTandRESUME"},
     { name: "Airbnb Dupe", description: "AirBnb Duplicate Website in works, using React Documention and freecodecamp to create this website dupe.", photo: "/ProjectImages/airbnb.png", codeLink: "https://github.com/jesus900000000/dupeofairbnb" },
     { name: "Personal CasaOS Linux Server", description: "Used for hobbies, file storage, communications, learning, among other cool things!", photo: "/ProjectImages/casaos.png", codeLink: "" },
-    { name: "Personal Portfolio (this site)", description: "Personal Website. Completely Designed, Developed, and Deployed by myself.", photo: "/ProjectImages/portfolio.png", codeLink: "" },
+    { name: "Personal Portfolio (this site)", description: "Personal Website. Completely Designed, Developed, and Deployed with a personal domain name by myself using industry leading tools and practices.", photo: "/ProjectImages/portfolio.png", codeLink: "https://github.com/jesus900000000/JesusGutierrezPortfolio" },
 ];
 
 
